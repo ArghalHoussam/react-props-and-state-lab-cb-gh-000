a3902a3a2c254cb035d129d23c631c0def7be67a
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-09 20:23:49 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -20,7 +20,7 @@ class Pet extends React.Component {
         <div className="extra content">
           <button className={ this.props.petInfo.isAdopted ? "ui button primary" : "ui button disabled" }>Already adopted</button>
           <button
-
+           onClick={this.props.onAdoptPet}
            className={ this.props.petInfo.isAdopted ? "ui button" : "ui button primary" }
            id={this.props.petInfo.id}>Adopt pet</button>
         </div>
